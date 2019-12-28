6a2a91e434c8cdfe73d56fe6c575b7233a6524b9
lavish-jain <lavish.jain.che15@iitbhu.ac.in>
2019-12-28 18:17:31 +0530

    Fixed url encoding issues

--- src/main/java/com/microland/plugins/PurgeUUIDFunction.java
+++ src/main/java/com/microland/plugins/PurgeUUIDFunction.java
@@ -1,7 +1,6 @@
 package com.microland.plugins;
 
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -14,7 +13,8 @@ import org.graylog.plugins.pipelineprocessor.ast.functions.FunctionArgs;
 import org.graylog.plugins.pipelineprocessor.ast.functions.FunctionDescriptor;
 import org.graylog.plugins.pipelineprocessor.ast.functions.ParameterDescriptor;
 
-import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.*;
 import java.util.Base64;
 
 
@@ -26,23 +26,22 @@ public class PurgeUUIDFunction implements Function<String> {
     public static final String NAME = "purge_hash";
 //    private static final String PARAM = "string";
 
-    private final ParameterDescriptor lookup_table_name = ParameterDescriptor
+    private final ParameterDescriptor<String, String> lookup_table_name = ParameterDescriptor
             .string("lookup_table_name")
             .description("Lookup table name")
             .build();
-    private final ParameterDescriptor hash = ParameterDescriptor
+    private final ParameterDescriptor<String, String> hash = ParameterDescriptor
             .string("hash")
             .description("Hash value to be purged")
             .build();
-    private final ParameterDescriptor baseUrl = ParameterDescriptor
+    private final ParameterDescriptor<String, String> baseUrl = ParameterDescriptor
             .string("base_url")
             .description("URL of the graylog instance")
             .build();
-    private final ParameterDescriptor key = ParameterDescriptor
+    private final ParameterDescriptor<String, String> key = ParameterDescriptor
             .string("key")
             .description("Auth Token")
             .build();
-    private String pwd = "token";
 
     @Override
     public Object preComputeConstantArgument(FunctionArgs functionArgs, String s, Expression expression) {
@@ -52,20 +51,40 @@ public class PurgeUUIDFunction implements Function<String> {
     @Override
     public String evaluate(FunctionArgs functionArgs, EvaluationContext evaluationContext) {
         HttpClient httpclient = HttpClients.createDefault();
-        String apiUrl  = baseUrl + "/api/system/lookup/tables/"+lookup_table_name+"/purge"+"?key="+hash;
-        String encoding = Base64.getEncoder().encodeToString((key.toString()+":"+pwd).getBytes());
-        HttpPost httpPost = new HttpPost(apiUrl);
+        String bUrl = baseUrl.required(functionArgs, evaluationContext);
+        String table = lookup_table_name.required(functionArgs, evaluationContext);
+        String hashVal = hash.required(functionArgs, evaluationContext);
+        String keyVal = key.required(functionArgs, evaluationContext);
+        String apiUrl  = bUrl + "/api/system/lookup/tables/"+table+"/purge"+"?key=";
+        URI uri = null;
+        try {
+            uri = new URI(apiUrl + URLEncoder.encode(hashVal, "UTF-8"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        System.out.println(uri);
+        String pwd = "token";
+        String encoding = Base64.getEncoder().encodeToString((keyVal+":"+ pwd).getBytes());
+        HttpPost httpPost = null;
+        try {
+            httpPost = new HttpPost(uri);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         httpPost.setHeader(HttpHeaders.AUTHORIZATION, "Basic " + encoding);
+        httpPost.setHeader("X-Requested-By", "client");
         HttpResponse response = null;
         try {
             response = httpclient.execute(httpPost);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        HttpEntity entity = response.getEntity();
+        assert response != null;
         if (response.getStatusLine().getStatusCode() > 299) {
+            System.out.println(response.getStatusLine().toString());
             return "FAILURE" + response.getStatusLine().toString();
         }
+//        System.out.println("Success");
         return "SUCCESS";
     }
     @Override
